fdbeb8e38ea262544cd1b07b634eb7a63a5dc00d
jhony <jhonatansouzameza100@gmail.com>
2026-03-20 02:33:42 -0300

    feat: ajustes de cleinte version 2

--- src/script.js
+++ src/script.js
@@ -454,7 +454,7 @@ const sr = ScrollReveal({
   duration: 2500
 })
 
-sr.reveal(`.home_img`, { opacity: 1, distance: '120px', delay: 400 })
+sr.reveal(`.home_img-animation`, { origin: 'top', opacity: 1, distance: '120px', delay: 400 })
 sr.reveal(`.home_badge-right, .testimonials_title, .footer_columns`, { origin: 'right', distance: '120px', delay: 400 })
 sr.reveal(`.products_description, .contact_description, .testimonials_description`, { delay: 600 })
 sr.reveal(`.home_badge-left, .products_grid, .faq_list`, { origin: 'left', distance: '120px', delay: 400 })
